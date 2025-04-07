3c762c11d0957a88ab8792bd18163eb7727bec99
heidi albarazi <heidi.albarazi@tum.de>
2025-04-07 20:28:57 +0200

    add different styling

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('index.html')
+
+@main.route('/task')
+def task():
+    return render_template('task.html')
+
+@main.route('/about')
+def about():
+    return render_template('about.html')
